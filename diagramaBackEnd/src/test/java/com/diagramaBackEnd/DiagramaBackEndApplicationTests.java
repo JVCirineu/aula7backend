ffc6e7a4d6fee8d952699a4497a274200a013c96
JVCirineu <132360356+JVCirineu@users.noreply.github.com>
2024-03-14 14:16:37 -0300

    Add files via upload

---
+++ diagramaBackEnd/src/test/java/com/diagramaBackEnd/DiagramaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diagramaBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagramaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
